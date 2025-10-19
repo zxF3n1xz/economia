64d2655fd0fc014f1f905517a953ded1a22c1b3c
Felipe Peres <felipepdoliveira@gmail.com>
2025-10-19 16:44:35 -0300

    Página final!

--- script.js
+++ script.js
@@ -3,47 +3,62 @@ const matrizGastos = [
     ["Transporte", 0],
     ["Lazer", 0],
     ["Outros", 0],
-    ["Total", 0],
-]
+    ["Total", 0]
+];
 //Funções utilitárias
+
 const obterElemento = (id) => document.getElementById(id);
-
-const valorNegativo = (valor) => valor < 0;
-const somaValor = (total, valor) => total + valor;
-
+const valorNegativo = () => valor < 0;
+const somaValor = (total, valor) => total+valor;
 const limparCampos = () => obterElemento('valor').value = '';
+const formataMoeda = (valor) => valor.toFixed(2).replace('.', ',')
 
-//Obter valores do formulário
-const obterValorInformado = () => obterElemento('valor');
+//obter valores do formulário
+
+const obterValorInformado = () => parseFloat(obterElemento('valor').value);
 const obterCategoriaInformada = () => obterElemento('categoria').value;
 
 //Obter categoria da matriz
-const obterCategoria = (matriz, nomeCategoria) => matriz.find((item) => item [0] === nomeCategoria);
+
+const obterCategoria = (matriz, nomeCategoria) => matriz.find((item) => item[0] === nomeCategoria);
 
 //Atualizar valores da matriz
-const atualizaValorCategoria = (categoria, valor) => categoria[1] = somaValor(categoria[1], valor);
 
-const atualizarInterface = () => {
+const atualizarValorCategoria = (categoria, valor) => categoria[1] = somaValor(categoria[1], valor);
 
+const atualizarInterface = () =>{
     matrizGastos.forEach(([nome, valor]) =>{
-        const elemento = obterElemento(nome)
-        elemento.textContent = `${nome}: R${valor}`
+        const elemento = obterElemento(nome);
+        elemento.textContent = `${nome}: R$${formataMoeda(valor)}`;
     })
+};
 
-}
 
+function adicionarGastos(){
+    //Obter categoria e valor
+    const valorInformado = obterValorInformado();
 
-function adicionarGastos() {
+    const categoriaInformada = obterCategoriaInformada();
 
-/*
-    1. pegar o valor informado
-    2. pegar categoria informada
-    3. impedir números negativos
-    4. de acordo com a categoria, atualiza o valor
-    4.1 criar variáveis para controlar ou armazenar os valores de cada uma das categorias
-    5. atualizar interface
-    6. limpar campos
-*/
+    //Bloquear os valores negativos
+
+    if (valorNegativo()){
+        alert("Valor inválido. O valor não pode ser negativo.");
+        return;
+    }
+
+    //Obter categoria da matriz
+
+    const categoria = obterCategoria(matrizGastos, categoriaInformada);
+
+    const total = obterCategoria(matrizGastos, "Total");
+
+    //Atualizar layout da página
+
+    atualizarValorCategoria(categoria, valorInformado);
+    atualizarValorCategoria(total, valorInformado);
+    atualizarInterface();
+    limparCampos();
 
 }
 
